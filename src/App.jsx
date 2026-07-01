fadca564490ba364d44d199a0c9d6ecd0cce0cd7
rimanshuu <rimanshudhapodkar@gmail.com>
2026-07-01 18:35:12 +1000

    Adds caching as optimization to reduce API calls.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import {  useState, useEffect } from "react";
+import {  useState, useEffect, useRef } from "react";
 import 'tachyons';
 import { useDebounce } from "./custom-hooks/useDebounce";
+//import { inCachedSearhes } from "./previousSearches";
 import SearchBox from './SearchBox';
 import CardList from "./CardList";
 
@@ -14,21 +15,41 @@ function App() {
 
     const debouncedInput = useDebounce(input);
 
-     useEffect (() => {
-        fetch(`https://dummyjson.com/users/search?q=${debouncedInput}`)
-        .then(response => response.json())
-        .then(data => {
-            setRobotArray(data.users)
-            setLoadingState(false)
-     })
-    }, [debouncedInput]);
+    const cacheRef = useRef(new Map())
 
-    // const filteredRobotArray = robotArray.filter((robot) => {
-    //     return(
-    //         robot.name.toLowerCase().includes(input.toLowerCase())
-    //     );
-    // })
-    
+    // a funciton to get cached input value to we can use it inside the useEffect
+    function getCachedInput(cache, input) {
+        return cache.current.get(input)
+    }
+
+    // A funciton to add input value into the cache
+    function setCachedInput (cache, input, result) {
+        cache.current.set(input, result)
+    }
+
+    useEffect (() => {
+
+        if (cacheRef.current.has(debouncedInput)) {
+            //get input and results from cached results
+            console.log("fetching from cache")
+
+            setRobotArray(getCachedInput(cacheRef, debouncedInput))
+            setLoadingState(false)
+
+        } else{
+
+            console.log("making api call")
+            
+            fetch(`https://dummyjson.com/users/search?q=${debouncedInput}`)
+            .then(response => response.json())
+            .then(data => {
+                //add new debouncedInput to cache here
+                setCachedInput(cacheRef, debouncedInput, data.users)
+                setRobotArray(data.users)
+                setLoadingState(false)
+            })
+        } 
+    }, [debouncedInput]);
 
     if (loadingState) {
         return (
